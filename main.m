5d5740ad93f2d0a6c2454a6e3b644962277046ad
Tobias Jansson <tobiasjansson07@gmail.com>
2023-11-16 21:59:33 +0100

    plots added

--- main.m
+++ main.m
@@ -9,7 +9,7 @@ D0 = 27.0002*10^-3; % Outer conductor diameter [m]
 % Dc = 1; % Conductor core diameter [m]
 Area = 27.0002*10^-3; % Projected area of conductor [m^2/m]
 
-Ts = 90; % Conductor surface temperature [°C]
+Ts = 80; % Conductor surface temperature [°C]
 
 % 566995:566995+8760
 Ta = tempdata(566995:566995+8759,3); % Ambient air temperature [°C]
@@ -26,12 +26,15 @@ He = 40; % Elevation of conductor above sea level [m]
 
 % 577612:577612+8759
 Vw = winddata(577612:577612+8759,5); % Wind velocity [m/s]
-phi = winddata(577612:577612+8759,3); % Angle between the wind direction and the conductor axis [deg]. Look at page 11 if issues occur.
+phi = mod(90+winddata(577612:577612+8759,3),360); % Angle between the wind direction and the conductor axis [deg]. Look at page 11 if issues occur.
 
 alpha = 0.8; % Solar absorptivity (.23 to .91) []
 epsilon = 0.8; % Emissivity (.23 to .91) []
 
 I = zeros(length(Ta),1);
+qc = zeros(length(Ta),1);
+qr = zeros(length(Ta),1);
+qs = zeros(length(Ta),1);
 for i=1:length(Ta)
     N = floor((i - 1) / 24) + 1; % Day of the year
     hour = mod(i - 1, 24); % Hour of the day
@@ -40,25 +43,29 @@ for i=1:length(Ta)
     % Natural convection heat loss
     Tfilm = (Ts+Ta(i))/2; % Average temperature of boundary layer [°C]
     rhof = (1.293-1.525*10^-4*He+6.379*10^-9*He^2)/(1+0.00367*Tfilm); % Air density [kg/m3]
-    qcn = 3.645*rhof^0.5*D0^0.75*(Ts-Ta(i))^1.25; % [W/m]
+    qcn(i) = 3.645*rhof^0.5*D0^0.75*(Ts-Ta(i))^1.25; % [W/m]
     
     % Forced convection
-    muf = (1.458*10^-6*(Tfilm+273)^1.5)/(Tfilm+383.4); % Absolute (dynamic) viscosity of air [kg/m-s]
-    kf = 2.424*10^-2+7.477*10^-5*Tfilm-4.407*10^-9*Tfilm^2; % Thermal conductivity of air at temperature Tfilm [W/(m-°C)
-    Kangle = 1.194-cosd(phi(i))+0.194*cosd(2*phi(i))+0.368*sind(2*phi(i));
-    NRe = (D0*rhof*Vw(i))/muf; % Reynolds number []. Transition between qc1 and qc2 at 1000, or compare and take highest?
-    if NRe<1000
-        qcf = Kangle*(1.01+1.35*NRe^0.52)*kf*(Ts-Ta(i)); % [W/m]
+    if Vw(i) == 0 % No wind
+        qcf(i) = 0;
     else
-        qcf = Kangle*0.754*NRe^0.6*kf*(Ts-Ta(i)); % [W/m]
+        muf = (1.458*10^-6*(Tfilm+273)^1.5)/(Tfilm+383.4); % Absolute (dynamic) viscosity of air [kg/m-s]
+        kf = 2.424*10^-2+7.477*10^-5*Tfilm-4.407*10^-9*Tfilm^2; % Thermal conductivity of air at temperature Tfilm [W/(m-°C)
+        Kangle(i) = 1.194-cosd(phi(i))+0.194*cosd(2*phi(i))+0.368*sind(2*phi(i));
+        NRe(i) = (D0*rhof*Vw(i))/muf; % Reynolds number []. Transition between qc1 and qc2 at 1000, or compare and take highest?
+        if NRe(i)<1000
+            qcf(i) = Kangle(i)*(1.01+1.35*NRe(i)^0.52)*kf*(Ts-Ta(i)); % [W/m]
+        else
+            qcf(i) = Kangle(i)*0.754*NRe(i)^0.6*kf*(Ts-Ta(i)); % [W/m]
+        end
     end
     
-    qcoptions = [qcn qcf];
+    qcoptions = [qcn(i) qcf(i)];
 
-    qc = max(qcoptions);
+    qc(i) = max(qcoptions);
     
     % Radiated heat loss
-    qr = 17.8*D0*epsilon*(((Ts+273)/100)^4-((Ta(i)+273)/100)^4); % [W/m]
+    qr(i) = 17.8*D0*epsilon*(((Ts+273)/100)^4-((Ta(i)+273)/100)^4); % [W/m]
     
     % Solar heat gain
     delta = 23.46*sind((284+N)/365*360); % Solar declination (–23.45 to +23.45) [deg]
@@ -97,19 +104,19 @@ for i=1:length(Ta)
         Qse = Ksolar*Qs; 
         
         theta = acosd(cosd(Hc)*cosd(Zc-Zl)); % Effective angle of incidence of the sun rays [deg]
-        qs = alpha*Qse*sind(theta)*Area; % Solar heat gain [W/m]
-        %qs = irradiation(i)*Area*alpha;
+        qs(i) = alpha*Qse*sind(theta)*Area; % Solar heat gain [W/m]
+        %qs(i) = irradiation(i)*Area*alpha;
     else
         % Sun is down, set solar heat gain to zero
-        qs = 0;
-        %qs = irradiation(i)*Area*alpha;
+        qs(i) = 0;
+        %qs(i) = irradiation(i)*Area*alpha;
     end
     
     % Conductor resistance
     R_Ts = ((R_Thigh-R_Tlow)/(Thigh-Tlow))*(Ts-Tlow)+R_Tlow; % AC resistance of conductor at temperature Tavg [ohm/m]
     
     % Ampacity
-    I(i) = sqrt((qc+qr-qs)/R_Ts); % [A]
+    I(i) = sqrt((qc(i)+qr(i)-qs(i))/R_Ts); % [A]
 end
 
 U = 400*10^3;
@@ -117,4 +124,54 @@ PF = 0.95;
 
 power = sqrt(3)*U*I*PF*10^-6;
 
+figure
 plot(1:length(Ta),power)
+ylim([0 3000])
+title('Transmission capacity during 2022')
+xlabel('Time [h]')
+ylabel('Capacity [MW]')
+
+figure
+plot(1:length(Ta),Ta)
+title('Ambient temperature during 2022')
+xlabel('Time [h]')
+ylabel('Temperature [C]')
+
+
+figure
+plot(1:length(Ta),Vw)
+title('Wind speed during 2022')
+xlabel('Time [h]')
+ylabel('Wind speed [m/s]')
+
+figure
+plot(1:length(Ta),phi)
+title('Wind direction during 2022')
+xlabel('Time [h]')
+ylabel('Wind direction [deg]')
+
+figure
+plot(1:length(Ta),irradiation)
+title('Solar irradiation during 2022')
+xlabel('Time [h]')
+ylabel('Irradiation [W/m^2]')
+
+figure
+plot(1:length(Ta),qc)
+title('Convection heat loss during 2022')
+xlabel('Time [h]')
+ylabel('qc [W/m]')
+
+figure
+plot(1:length(Ta),qr)
+title('Radiative heat loss during 2022')
+xlabel('Time [h]')
+ylabel('qr [W/m]')
+
+figure
+plot(1:length(Ta),qs)
+title('Solar heat gain during 2022')
+xlabel('Time [h]')
+ylabel('qs [W/m]')
+
+
