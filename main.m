233f852eea4e9e7fa788539d12eb673ba60a7755
Tobias Jansson <tobiasjansson07@gmail.com>
2023-11-16 15:23:23 +0100

    solar added

--- main.m
+++ main.m
@@ -3,6 +3,7 @@ close all
 %% Setup
 tempdata = readmatrix('temp.csv'); 
 winddata = readmatrix('wind.csv');
+solardata = readmatrix('solar.csv');
 
 D0 = 27.0002*10^-3; % Outer conductor diameter [m]
 % Dc = 1; % Conductor core diameter [m]
@@ -10,27 +11,31 @@ Area = 27.0002*10^-3; % Projected area of conductor [m^2/m]
 
 Ts = 90; % Conductor surface temperature [°C]
 
-Ta = tempdata(566995:566995+8760,3); % Ambient air temperature [°C]
+% 566995:566995+8760
+Ta = tempdata(566995:566995+8759,3); % Ambient air temperature [°C]
 Tlow = 25; % Low average conductor temperature for which ac resistance is specified [°C]
 Thigh = 100; % High average conductor temperature for which ac resistance is specified [°C]
 R_Tlow = 0.0736325*10^-3 ; % Lower temperature resistance [ohm/m]
 R_Thigh = 0.088359*10^-3; % Higher temperature resistance [ohm/m]
 
+% 341881:341881+8759
+irradiation = solardata(341881:341881+8759,3);
 Zl = 90; % Azimuth of line [deg]
 Lat = 65; % Degrees of latitude [deg]
 He = 40; % Elevation of conductor above sea level [m]
 
-Vw = winddata(566995:566995+8760,5); % Wind velocity [m/s]
-phi = winddata(566995:566995+8760,3); % Angle between the wind direction and the conductor axis [deg]. Look at page 11 if issues occur.
+% 577612:577612+8759
+Vw = winddata(577612:577612+8759,5); % Wind velocity [m/s]
+phi = winddata(577612:577612+8759,3); % Angle between the wind direction and the conductor axis [deg]. Look at page 11 if issues occur.
 
 alpha = 0.8; % Solar absorptivity (.23 to .91) []
 epsilon = 0.8; % Emissivity (.23 to .91) []
 
 I = zeros(length(Ta),1);
 for i=1:length(Ta)
-    N(i) = floor((i - 1) / 24) + 1; % Day of the year
-    hour(i) = mod(i - 1, 24); % Hour of the day
-    omega = 15 * (hour(i) - 12); % Hour angle relative to noon [deg]. 15*(Time-12), at 11AM, Time = 11 and the Hour angle= –15 deg
+    N = floor((i - 1) / 24) + 1; % Day of the year
+    hour = mod(i - 1, 24); % Hour of the day
+    omega = 15 * (hour - 12); % Hour angle relative to noon [deg]. 15*(Time-12), at 11AM, Time = 11 and the Hour angle= –15 deg
     
     % Natural convection heat loss
     Tfilm = (Ts+Ta(i))/2; % Average temperature of boundary layer [°C]
@@ -56,10 +61,10 @@ for i=1:length(Ta)
     qr = 17.8*D0*epsilon*(((Ts+273)/100)^4-((Ta(i)+273)/100)^4); % [W/m]
     
     % Solar heat gain
-    delta(i) = 23.46*sind((284+N(i))/365*360); % Solar declination (–23.45 to +23.45) [deg]
-    Hc(i) = asind(cosd(Lat)*cosd(delta(i))*cosd(omega)+sind(Lat)*sind(delta(i))); % Altitude of sun [deg]
-    if Hc(i) > 1 % Sun is above the horizon
-        chi = sind(omega)/(sind(Lat)*cosd(omega)-cosd(Lat)*tand(delta(i))); % Solar azimuth variable []
+    delta = 23.46*sind((284+N)/365*360); % Solar declination (–23.45 to +23.45) [deg]
+    Hc = asind(cosd(Lat)*cosd(delta)*cosd(omega)+sind(Lat)*sind(delta)); % Altitude of sun [deg]
+    if Hc > 1 % Sun is above the horizon
+        chi = sind(omega)/(sind(Lat)*cosd(omega)-cosd(Lat)*tand(delta)); % Solar azimuth variable []
         if -180 <= omega && omega < 0
             if chi >= 0
                 C=0; % Solar azimuth constant [deg]
@@ -83,26 +88,28 @@ for i=1:length(Ta)
         E = -3.61118e-4;
         F = 1.94318e-6;
         G = -4.07608e-9;
-        Qs = A + B*Hc(i) + C*Hc(i)^2 + D*Hc(i)^3 + E*Hc(i)^4 + F*Hc(i)^5 + G*Hc(i)^6;
+        Qs = A + B*Hc + C*Hc^2 + D*Hc^3 + E*Hc^4 + F*Hc^5 + G*Hc^6;
         
         a = 1;
         b = 1.148*10^-4;
         c = -1.108*10^-8;
         Ksolar = a + b*He + c*He^2; % Solar altitude correction factor []
-        Qse(i) = Ksolar*Qs; 
+        Qse = Ksolar*Qs; 
         
-        theta(i) = acosd(cosd(Hc(i))*cosd(Zc-Zl)); % Effective angle of incidence of the sun rays [deg]
-        qs(i) = alpha*Qse(i)*sind(theta(i))*Area; % Solar heat gain [W/m]
+        theta = acosd(cosd(Hc)*cosd(Zc-Zl)); % Effective angle of incidence of the sun rays [deg]
+        qs = alpha*Qse*sind(theta)*Area; % Solar heat gain [W/m]
+        %qs = irradiation(i)*Area*alpha;
     else
         % Sun is down, set solar heat gain to zero
-        qs(i) = 0;
+        qs = 0;
+        %qs = irradiation(i)*Area*alpha;
     end
     
     % Conductor resistance
     R_Ts = ((R_Thigh-R_Tlow)/(Thigh-Tlow))*(Ts-Tlow)+R_Tlow; % AC resistance of conductor at temperature Tavg [ohm/m]
     
     % Ampacity
-    I(i) = sqrt((qc+qr-qs(i))/R_Ts); % [A]
+    I(i) = sqrt((qc+qr-qs)/R_Ts); % [A]
 end
 
 U = 400*10^3;
@@ -110,3 +117,4 @@ PF = 0.95;
 
 power = sqrt(3)*U*I*PF*10^-6;
 
+plot(1:length(Ta),power)
